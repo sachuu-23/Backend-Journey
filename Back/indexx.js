bb4835eff697d1ca3b436c0f85f2a304496a9832
Sachin <sachinmizo1223@gmail.com>
2026-01-06 19:27:07 +0530

    Learned about http Module of Node js

--- Back/indexx.js
+++ Back/indexx.js
@@ -1,20 +1,33 @@
-const indexx = require("./server");
-console.log(indexx.sum(10,30));
-console.log(indexx.sub(30,10))
+// const http = require('http');
 
-const array = [1,2,3,4];
-const target = 7;
+// // const servers = http.createServer((req,res)=>{
+// //     res.write("hello");
+// //     res.end();//this is used to end the server req and res for this specific server 
+// // })
 
-try{
-    let result = indexx.divide(10,2);
-        if(result <3){
-            console.log("Hi there");
-        }
-        else{
-            console.log("Good Bye");        }
-}
-catch(error){
-    console.log("The error is :",error.message);
+const http = require('http');
+const server = http.createServer((req,res) =>{
+    console.log(req.url);
+    console.log(req.headers);
+    res.writeHead(200,{"Content-Type":"text/plain"})
+    res.end("I am the Best Giga brain");
+        })
 
-}
+//WE need a port on which our server will listen the request and send back the response
+
+const port = 3000;
+server.listen(port ,()=>{
+    console.log(`The server is running on port ${port}`);
+
+})//this return a call back , as this is happening outside of node environment and port 
+//is listenning some where else , and i will need confirmation that this port is running or not 
+//by logging it out, with the logic of placeholder, which put that value only whcih is current right now
+
+
+//this simply end the response process for current client request, means
+    //if for one request the response is completed then simply end the res by using res.end() keyword 
+
+
+
+
 
