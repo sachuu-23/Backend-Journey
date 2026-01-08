a80a2ba742499e0f1ca49d5d5f2be608c7e9f26d
Sachin <sachinmizo1223@gmail.com>
2026-01-08 20:12:05 +0530

    I learned how to use .then and catch which takes two inputs as if resolve then send it in .then and if it is .catch then it is generally for error only

--- Back/promises.js
+++ Back/promises.js
@@ -1,13 +1,36 @@
 // First lets understand what is promise , promise is a type of call back function which means that i will run later on, wait for a moment, basically means wait as it will return something, if that thing get resolve then it will return resolve or else it wil return reject , resolve is a type of call back function which indicates that data is fetched and we got the required data.
 
-function delayTime(time){
-    return new Promise((resolve)=>setTimeout(resolve,time));
+// function delayTime(time){
+//     return new Promise((resolve)=>setTimeout(resolve,time));
+// }
+
+
+// // when we return the promise we do something from that returned value which means ,do some action , when prmoise is returned , for that we use .then , means once the promise is returned do this stuffs now , you can make use of the returned data or you can print that data or agian call something other and send that recive data there 
+// console.log("Promise stars from here");
+
+// delayTime(4000).then(()=>console.log("The other promise execute after 4 seconds"));
+
+// console.log("end");
+
+
+
+
+//We are checking if num2 === 0 which means its an undefined number right in maths which means that it is inreject case, and yeah you do have to provide the reason for the rejct also 
+function divideFunction(num1,num2){
+    return new Promise((resolve, reject)=>{
+        if(num2 === 0){
+            reject('The number cannot be divisible by 0');
+        }
+       else{
+        resolve(num1/num2);
+       }
+    })
 }
 
 
-// when we return the promise we do something from that returned value which means ,do some action , when prmoise is returned , for that we use .then , means once the promise is returned do this stuffs now , you can make use of the returned data or you can print that data or agian call something other and send that recive data there 
-console.log("Promise stars from here");
-
-delayTime(4000).then(()=>console.log("The other promise execute after 4 seconds"));
-
-console.log("end");
+divideFunction(10,0).then(result=>{
+    console.log(result);
+})
+    .catch(error=>{
+        console.log(error,'is our error');
+    })
